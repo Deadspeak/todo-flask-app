5a29888a1597cb0cb325efb06fab2fcf44817832
Marko <46193604+Deadspeak@users.noreply.github.com>
2025-05-12 11:52:20 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,8 @@ def create_app():
     app = Flask(__name__)
 
     # Konfiguracja bazy danych
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/tasks.db'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/tasks.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@todo-flask-server.database.windows.net:1433/tasksdb?driver=ODBC+Driver+17+for+SQL+Server&authentication=ActiveDirectoryMsi'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)  # Inicjalizujemy db z aplikacją
